926d800d42e49249d88c00e6ba4dd94cc4652485
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-17 12:31:53 +0000

    feat: add rocksalt font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      rocksalt: ["Rock Salt", "cursive"],
+    },
   },
   plugins: [],
 };
