9ad62119984d78287ca6b57426bd7dc444a88316
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-17 14:03:07 +0000

    feat: add rock salt font and red color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    fontFamily: {
-      rocksalt: ["Rock Salt", "cursive"],
-    },
-    colors: {
-      "red-mistermenu": "#71130B",
+    extend: {
+      fontFamily: {
+        rocksalt: ["Rock Salt", "cursive"],
+      },
+      colors: {
+        "red-mistermenu": "#71130B",
+      },
     },
   },
   plugins: [],
