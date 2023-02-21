e44f1006a6b38253ffec96a3b0b48837e57abfd9
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-21 22:19:55 +0800

    fix {} expressions

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,16 +1,16 @@
 import { apiGetProductImageById } from "../api/products";
 import { Counter } from "./button";
 
-export function Cards(props) {
+export function Cards() {
   return (
     <div className="h-96 w-72 overflow-hidden rounded-xl bg-rose-50 shadow-2xl">
       <img
         className="h-52 w-full object-cover"
-        src={props.productImageId}
-        alt={props.name}
+        src={products.image}
+        alt={products.name}
       />
-      <h2 className="m-5 text-2xl">{props.name}</h2>
-      <p className="m-5 my-0 mx-5 mb-5	text-xl">{props.description}</p>
+      <h2 className="m-5 text-2xl">{}</h2>
+      <p className="m-5 my-0 mx-5 mb-5	text-xl">{products.name}</p>
       <Counter>Add to Cart</Counter>
     </div>
   );
