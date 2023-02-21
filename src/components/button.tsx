ea043f77ca9da8dbddd8bb2c1d5b08ecf5a0acf1
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-21 21:19:16 +0800

    trying to get product image by id

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -17,10 +17,16 @@ export function Counter() {
       <p>
         <apiGetProductsById />: {counter}
       </p>
-      <button onClick={increment} className="bg-teal-500 p-2">
+      <button
+        onClick={increment}
+        className="m-auto block cursor-pointer rounded-3xl border-0 bg-rose-800 p-2 py-2.5 px-5 text-base	text-white	"
+      >
         +
       </button>
-      <button onClick={decrement} className="bg-teal-500 p-2">
+      <button
+        onClick={decrement}
+        className="m-auto block cursor-pointer rounded-3xl border-0 bg-rose-800 p-2 py-2.5 px-5 text-base	text-white	"
+      >
         -
       </button>
     </div>
--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -4,9 +4,13 @@ import { Counter } from "./button";
 export function Cards(props) {
   return (
     <div className="h-96 w-72 overflow-hidden rounded-xl bg-rose-50 shadow-2xl">
-      <img src={props.productImageId} alt={props.name} />
-      <h2>{props.name}</h2>
-      <p>{props.description}</p>
+      <img
+        className="h-52 w-full object-cover"
+        src={props.productImageId}
+        alt={props.name}
+      />
+      <h2 className="m-5 text-2xl">{props.name}</h2>
+      <p className="m-5 my-0 mx-5 mb-5	text-xl">{props.description}</p>
       <Counter>Add to Cart</Counter>
     </div>
   );
