aecf5169d2f834a3ea5c4dcd6efc8e76d46aa549
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-22 22:27:11 +0800

    add url

--- src/routes/recommendation.tsx
+++ src/routes/recommendation.tsx
@@ -1,4 +1,35 @@
+import { useLoaderData } from "react-router-dom";
+import { apiGetProducts } from "../api/products";
 
-export function RecomendationRoute() {
-  return <div>Rekomendasi menu disini</div>;
+export async function loader() {
+  const products = await apiGetProducts();
+  return { products };
+}
+
+export function RecommendationRoute() {
+  const { products } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
+
+  const randomIndex = Math.floor(Math.random() * products.length);
+  console.log(randomIndex);
+
+  return (
+    <div className="m-6 grid grid-cols-4 gap-6">
+      {products.map((product) => (
+        <div className="rounded-xl shadow-lg" key={product.id}>
+          <picture>
+            <img className="rounded-t-xl" src={product.image} />
+          </picture>
+          <h1 className="mt-4 mb-2 px-4 text-xl font-bold">{product.name}</h1>
+          <div className="my-4 flex justify-between pr-6">
+            <p className="px-4 text-xl font-bold text-gray-500">
+              $ {product.price}
+            </p>
+            <button className="rounded-lg bg-red-800 px-8 text-white">
+              ADD <span className="text-lg font-extrabold">+</span>
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
