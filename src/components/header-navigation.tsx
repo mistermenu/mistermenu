d96cf778572fea262bcbf9b1dc205ae178060488
Abdehi Perdana Ain <abdehiperdana@gmail.comabdehiperdana@gmail.com>
2023-02-22 21:30:20 +0800

    delete public and refactor name link to

--- src/components/header-navigation.tsx
+++ src/components/header-navigation.tsx
@@ -12,7 +12,7 @@ export function HeaderNavigation() {
       </div>
       <div className="flex h-56 w-full justify-between bg-slate-400 px-40 py-4">
         <div className=" w-36 rounded-sm  border-[3px] border-gray-200 bg-white text-center shadow-2xl hover:border-red-200 hover:bg-red-200">
-          <Link to="/recomendationmenu">
+          <Link to="/recomendation">
             <img className="p-3" src="/assets/images/Rekomendasi.png" alt="" />
             <h1 className="text-sm font-semibold text-gray-600">
               RECOMENDATION
@@ -20,31 +20,31 @@ export function HeaderNavigation() {
           </Link>
         </div>
         <div className=" w-36 rounded-sm  border-[3px] border-gray-200 bg-white text-center shadow-2xl hover:border-red-200 hover:bg-red-200">
-          <Link to="/chickenmenu">
+          <Link to="/chicken">
             <img className="p-3" src="/assets/images/AYAM.png" alt="" />
             <h1 className="text-sm font-semibold text-gray-600">CHICKEN</h1>
           </Link>
         </div>
         <div className=" w-36 rounded-sm  border-[3px] border-gray-200 bg-white text-center shadow-2xl hover:border-red-200 hover:bg-red-200">
-          <Link to="/porridgemenu">
+          <Link to="/porridge">
             <img className="p-3" src="/assets/images/BUBUR.png" alt="" />
             <h1 className="text-sm font-semibold text-gray-600">PORRIDGE</h1>
           </Link>
         </div>
         <div className=" w-36 rounded-sm  border-[3px] border-gray-200 bg-white text-center shadow-2xl hover:border-red-200 hover:bg-red-200">
-          <Link to="/seafoodmenu">
+          <Link to="/seafood">
             <img className="p-3" src="/assets/images/SEAFOOD.png" alt="" />
             <h1 className="text-sm font-semibold text-gray-600">SEAFOOD</h1>
           </Link>
         </div>
         <div className=" w-36 rounded-sm  border-[3px] border-gray-200 bg-white text-center shadow-2xl hover:border-red-200 hover:bg-red-200">
-          <Link to="/soupmenu">
+          <Link to="/soup">
             <img className="p-3" src="/assets/images/SUP.png" alt="" />
             <h1 className="text-sm font-semibold text-gray-600">SOUP</h1>
           </Link>
         </div>
         <div className=" w-36 rounded-sm  border-[3px] border-gray-200 bg-white text-center shadow-2xl hover:border-red-200 hover:bg-red-200">
-          <Link to="/drinkmenu">
+          <Link to="/drink">
             <img className="p-3" src="/assets/images/MINUMAN.png" alt="" />
             <h1 className="text-sm font-semibold text-gray-600">DRINK</h1>
           </Link>
