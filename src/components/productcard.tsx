5372fba1b56c5f2165ba9cf9095de9a451624b75
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-23 16:33:03 +0000

    feat: product card responsive in all devices

--- src/components/productcard.tsx
+++ src/components/productcard.tsx
@@ -9,19 +9,21 @@ type CardProp = {
 
 export function ProductCard({ image, name, price }: CardProp) {
   return (
-    <div className="mx-auto max-w-md overflow-hidden rounded-3xl shadow-lg">
+    <div className=" overflow-hidden rounded-3xl shadow-lg">
       <img className="w-full" src={image} alt={name} />
 
       <div className="px-6 py-4">
-        <p className="mb-2 text-xl font-bold">{name}</p>
+        <p className="mb-2 text-xl font-bold  lg:text-lg 2xl:mb-4 2xl:text-2xl">
+          {name}
+        </p>
 
-        <div className="flex w-full items-center justify-between py-2">
-          <p className="font-roboto mb-2 text-2xl font-bold text-gray-400 ">
+        <div className=" flex w-full flex-wrap items-center justify-between 2xl:flex-row">
+          <p className="font-roboto mb-2 text-lg font-bold text-gray-400 lg:text-lg 2xl:text-2xl ">
             {formatCurrency(price)}
           </p>
-          <button className="flex items-center gap-1 rounded-lg bg-red-mistermenu px-10 py-2 font-bold text-white hover:bg-red-700">
+          <button className="flex items-center gap-1 rounded-lg  bg-red-mistermenu px-10 py-2 font-bold text-white hover:bg-red-700 ">
             <p>ADD</p>
-            <TiPlus className="inline-block text-2xl" />
+            <TiPlus className=" 2xl:text-2xl" />
           </button>
         </div>
       </div>
