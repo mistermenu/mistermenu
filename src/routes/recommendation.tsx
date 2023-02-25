47e0ef711303576f4a8fb63e4f5fb18464fda065
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-25 08:57:15 +0800

    recommendation works: only show isRecommended: true

--- src/routes/recommendation.tsx
+++ src/routes/recommendation.tsx
@@ -2,44 +2,29 @@ import { ProductCard } from "../components/productcard";
 import { apiGetProducts } from "../api/products";
 import { useLoaderData } from "react-router-dom";
 import { Link } from "react-router-dom";
-import Chance from "chance";
-import PropTypes from "prop-types";
 
 export async function loader() {
   const products = await apiGetProducts();
   return { products };
 }
 
-interface Product {
-  id: string;
-  name: string;
-  image: string;
-  price: number;
-}
-
-export function ProductList({ products }) {
-  const chance = new Chance();
-  const randomProducts = chance.pickset(
-    { products },
-    chance.integer({ min: 1, max: { products }.length })
+export function RecomendationRoute() {
+  const { products } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
+  const recommendedProducts = products.filter(
+    (product) => product.isRecommended
   );
 
   return (
     <div className="m-6 grid grid-cols-4 gap-6">
-      {products.map((product) => (
-        <div className="rounded-xl shadow-lg" key={product.id}>
-          <picture>
-            <img className="rounded-t-xl" src={product.image} />
-          </picture>
-          <h1 className="mt-4 mb-2 px-4 text-xl font-bold">{product.name}</h1>
-          <div className="my-4 flex justify-between pr-6">
-            <p className="px-4 text-xl font-bold text-gray-500">
-              $ {product.price}
-            </p>
-            <button className="rounded-lg bg-red-800 px-8 text-white">
-              ADD <span className="text-lg font-extrabold">+</span>
-            </button>
-          </div>
+      {recommendedProducts.map((product) => (
+        <div key={product.id}>
+          <Link to={`/product/${product.name}`}>
+            <ProductCard
+              image={product.image}
+              name={product.name}
+              price={product.price}
+            />
+          </Link>
         </div>
       ))}
     </div>
