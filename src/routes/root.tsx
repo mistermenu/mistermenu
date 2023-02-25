33c243a484a1546dab335a51ec0eb94990485243
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-25 11:37:43 +0000

    feat: wrap the return code with <ShoppingCartProvider> context

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -4,35 +4,39 @@ import { FaSpinner } from "react-icons/fa";
 import { HeaderNavigation } from "../components/header-navigation";
 
 import { FloatingCart } from "../components/floating-cart";
+import { ShoppingCartProvider } from "../context/ShoppingCartContext";
 
 export function RootRoute() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="bg-white">
-      <HeaderNavigation />
-      {isLoading && (
-        <main className="flex items-center justify-center p-10">
-          <FaSpinner className="animate-spin text-4xl" />
-        </main>
-      )}
-      {!isLoading && (
-        <main>
-          <Outlet />
-        </main>
-      )}
-      <footer className="bg-gray-200 p-5 text-center font-newscycle font-bold text-black">
-        <FloatingCart />
-        <p>
-          © Make with ❤️ By <span className="tracking-widest">MISTERMENU</span>{" "}
-          <span className="bg-gradient-to-b from-violet-200 to-violet-800 bg-clip-text text-transparent">
-            .AI
-          </span>
-        </p>
-      </footer>
+    <ShoppingCartProvider>
+      <div className="bg-white">
+        <HeaderNavigation />
+        {isLoading && (
+          <main className="flex items-center justify-center p-10">
+            <FaSpinner className="animate-spin text-4xl" />
+          </main>
+        )}
+        {!isLoading && (
+          <main>
+            <Outlet />
+          </main>
+        )}
+        <footer className="bg-gray-200 p-5 text-center font-newscycle font-bold text-black">
+          <FloatingCart />
+          <p>
+            © Make with ❤️ By{" "}
+            <span className="tracking-widest">MISTERMENU</span>{" "}
+            <span className="bg-gradient-to-b from-violet-200 to-violet-800 bg-clip-text text-transparent">
+              .AI
+            </span>
+          </p>
+        </footer>
 
-      <ScrollRestoration />
-    </div>
+        <ScrollRestoration />
+      </div>
+    </ShoppingCartProvider>
   );
 }
