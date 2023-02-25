5a8eeac15f7a0f866169e578c486da06521c8a94
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-25 04:59:04 +0000

    refactor: styling cart page

--- src/routes/cart.tsx
+++ src/routes/cart.tsx
@@ -3,6 +3,7 @@ import { apiGetProducts } from "../api/products";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { FaChevronLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export async function loader() {
   const products = await apiGetProducts();
@@ -23,34 +24,40 @@ export function CartRoute() {
 export function CartHeading() {
   return (
     <>
-      <header className="flex p-5 shadow-xl">
-        <nav className="mx-auto flex w-full max-w-6xl items-center justify-between">
-          <div>
-            <Link to="/">
-              {" "}
-              <FaChevronLeft className="text-3xl" />
-            </Link>
-          </div>
-
-          <div className="flex flex-col items-center">
-            <div className="flex items-center gap-2">
-              <AiOutlineShoppingCart className="inline text-2xl text-red-mistermenu xl:text-4xl" />
-              <h1 className=" font-sans text-2xl font-extrabold xl:text-3xl">
-                ORDER CART{" "}
-              </h1>
-            </div>
-          </div>
-
-          <div></div>
-        </nav>
-      </header>
+      <Header />
+      <div className="flex justify-center p-5">
+        <div className="flex items-center gap-2">
+          <AiOutlineShoppingCart className="inline text-3xl text-red-mistermenu xl:text-4xl" />
+          <h1 className="text-3xl  font-extrabold">ORDER CART</h1>
+        </div>
+      </div>
     </>
+  );
+}
+
+export function Header() {
+  const navigate = useNavigate();
+
+  return (
+    <header className="flex p-5">
+      <nav className="mx-auto flex w-full max-w-6xl items-center justify-between">
+        <button onClick={() => navigate(-1)}>
+          <FaChevronLeft className="text-3xl" />
+        </button>
+
+        <Link to={"/"}>
+          <h1 className=" font-rocksalt   text-red-mistermenu ">MisterMenu </h1>
+        </Link>
+
+        <div></div>
+      </nav>
+    </header>
   );
 }
 
 export function CartItems() {
   return (
-    <section className="my-10 mx-auto flex max-w-6xl rounded-md border border-gray-300 p-5 pt-8">
+    <section className="my-4 mx-auto flex max-w-6xl rounded-md border border-gray-300 p-5 pt-8">
       <div>
         <p>ORDER ITEM(S)</p>
       </div>
