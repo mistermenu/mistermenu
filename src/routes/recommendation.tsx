f4d2d29b402211da1d6405d5af831342c7d840d9
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-25 09:16:05 +0800

    fix: add idUrl below ProductCard

--- src/routes/recommendation.tsx
+++ src/routes/recommendation.tsx
@@ -1,18 +1,11 @@
 import { ProductCard } from "../components/productcard";
 import { apiGetProducts } from "../api/products";
-<<<<<<< HEAD
-import { ProductCard } from "../components/productcard";
-=======
 import { useLoaderData } from "react-router-dom";
 import { Link } from "react-router-dom";
->>>>>>> recommendation-semifinal
 
 export async function loader() {
   const products = await apiGetProducts();
-  const recommendedProducts = products.filter(
-    (product) => product.isRecommended === true
-  );
-  return { products: recommendedProducts };
+  return { products };
 }
 
 export function RecomendationRoute() {
@@ -22,28 +15,17 @@ export function RecomendationRoute() {
   );
 
   return (
-<<<<<<< HEAD
-    <div className="m-6 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {products.map((product) => (
-        <div key={product.id}>
-          <ProductCard
-            idUrl={`products/${product.id}`}
-            image={product.image}
-            name={product.name}
-            price={product.price}
-          />
-=======
     <div className="m-6 grid grid-cols-4 gap-6">
       {recommendedProducts.map((product) => (
         <div key={product.id}>
           <Link to={`/product/${product.name}`}>
             <ProductCard
+              idUrl={`${product.id}`}
               image={product.image}
               name={product.name}
               price={product.price}
             />
           </Link>
->>>>>>> recommendation-semifinal
         </div>
       ))}
     </div>
