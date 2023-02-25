f9cb190afff7c8ec052a4303c391ddfb29a043e2
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-25 23:44:34 +0800

    wrap a new Footer component, cart-floating, and show-cart-floating eventListener

--- src/components/cart-floating.tsx
+++ src/components/cart-floating.tsx
@@ -1,13 +1,17 @@
-import { useLoaderData } from "react-router-dom";
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export function FloatingCart() {
   return (
     <Link to="./cart">
-      <div className="fixed bottom-0 left-0 w-full p-3">
-        <div className="static rounded-lg bg-red-mistermenu opacity-50 hover:opacity-100">
-          <h1 className="p-5 text-center text-white ">Cart</h1>
+      <div className="fixed bottom-10 left-0 w-full px-[900px] drop-shadow-[0_-50px_50px_#E5E7EB] transition-all">
+        <div className="static flex justify-center rounded-lg bg-red-mistermenu ">
+          <h1 className="flex items-center p-5 text-white">
+            <span className="mx-2">
+              <FaShoppingCart />
+            </span>
+            Cart
+          </h1>
         </div>
       </div>
     </Link>
