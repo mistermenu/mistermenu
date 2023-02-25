af0448f79819170c3eb16c429794a9508f47bafd
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-25 09:47:17 +0800

    fix: product detail link only in product.image

--- src/routes/recommendation.tsx
+++ src/routes/recommendation.tsx
@@ -18,14 +18,12 @@ export function RecomendationRoute() {
     <div className="m-6 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {recommendedProducts.map((product) => (
         <div key={product.id}>
-          <Link to={`/product/${product.name}`}>
-            <ProductCard
-              idUrl={`${product.id}`}
-              image={product.image}
-              name={product.name}
-              price={product.price}
-            />
-          </Link>
+          <ProductCard
+            idUrl={`${product.id}`}
+            image={product.image}
+            name={product.name}
+            price={product.price}
+          />
         </div>
       ))}
     </div>
