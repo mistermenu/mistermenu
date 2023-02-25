24ffa67e1b43d9f6a63d11484beb5c98a382dc18
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-25 05:16:23 +0000

    refactor: w-full to w-49/50

--- src/components/floating-cart.tsx
+++ src/components/floating-cart.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export function FloatingCart() {
   return (
     <Link to="./cart">
-      <div className="fixed left-2 bottom-2 w-full rounded-lg bg-red-mistermenu opacity-50 hover:opacity-100">
+      <div className="fixed left-2 bottom-2 w-49/50 rounded-lg bg-red-mistermenu opacity-50 hover:opacity-100">
         <h1 className="p-5 text-center text-white ">Cart</h1>
       </div>
     </Link>
