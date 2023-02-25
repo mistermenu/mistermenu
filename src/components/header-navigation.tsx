181d2618d52d0aefb1fefff98e70060313d02f44
Abdehi Perdana Ain <abdehiperdana@gmail.comabdehiperdana@gmail.com>
2023-02-26 00:42:41 +0800

    change header navigation responsive

--- src/components/header-navigation.tsx
+++ src/components/header-navigation.tsx
@@ -5,18 +5,18 @@ export function HeaderNavigation() {
 
   return (
     <header>
-      <div className="flex w-full justify-center overflow-x-auto bg-slate-400 py-4 ">
+      <div className="flex w-full overflow-x-auto bg-slate-200 py-8 outline-violet-400 lg:justify-center">
         <div>
           <NavLink to="/recomendation">
             {({ isActive }) => (
-              <div className="border-1 border-gray-200 bg-white text-center shadow-2xl hover:border-red-200 hover:bg-red-200 md:rounded-sm md:border-[3px]">
+              <div className="border-1 h-46 w-40 border-gray-200 bg-white text-center shadow-2xl hover:border-red-200 hover:bg-red-200 md:rounded-sm md:border-[3px]">
                 <div className={isActive ? activeStyle : undefined}>
                   <img
                     className="p-3"
                     src="/assets/images/Rekomendasi.png"
                     alt=""
                   />
-                  <h1 className="overflow-x-auto text-xs font-semibold text-gray-600 sm:text-sm md:text-base lg:text-lg">
+                  <h1 className="overflow-x-auto text-lg font-semibold text-gray-600">
                     RECOMMENDATION
                   </h1>
                 </div>
@@ -28,10 +28,10 @@ export function HeaderNavigation() {
         <div>
           <NavLink to="/chicken">
             {({ isActive }) => (
-              <div className="border-1 border-gray-200 bg-white  text-center shadow-2xl hover:border-red-200 hover:bg-red-200 md:rounded-sm md:border-[3px]">
+              <div className="border-1 h-46 w-40 border-gray-200 bg-white  text-center shadow-2xl hover:border-red-200 hover:bg-red-200 md:rounded-sm md:border-[3px]">
                 <div className={isActive ? activeStyle : undefined}>
                   <img className="p-3" src="/assets/images/AYAM.png" alt="" />
-                  <h1 className="text-xs font-semibold text-gray-600 sm:text-sm md:text-base lg:text-lg">
+                  <h1 className="text-lg font-semibold text-gray-600">
                     CHICKEN
                   </h1>
                 </div>
@@ -42,10 +42,10 @@ export function HeaderNavigation() {
         <div>
           <NavLink to="/porridge">
             {({ isActive }) => (
-              <div className="border-1 border-gray-200 bg-white  text-center shadow-2xl hover:border-red-200 hover:bg-red-200 md:rounded-sm md:border-[3px]">
+              <div className="h-46 border-1 w-40 border-gray-200 bg-white  text-center shadow-2xl hover:border-red-200 hover:bg-red-200 md:rounded-sm md:border-[3px]">
                 <div className={isActive ? activeStyle : undefined}>
                   <img className="p-3" src="/assets/images/BUBUR.png" alt="" />
-                  <h1 className="text-xs font-semibold text-gray-600 sm:text-sm md:text-base lg:text-lg">
+                  <h1 className="text-lg font-semibold text-gray-600">
                     PORRIDGE
                   </h1>
                 </div>
@@ -53,33 +53,23 @@ export function HeaderNavigation() {
             )}
           </NavLink>
         </div>
-        <div>
-          <NavLink to="/seafood">
-            {({ isActive }) => (
-              <div className="border-1 border-gray-200 bg-white  text-center shadow-2xl hover:border-red-200 hover:bg-red-200 md:rounded-sm md:border-[3px]">
-                <div className={isActive ? activeStyle : undefined}>
-                  <img
-                    className="p-3"
-                    src="/assets/images/SEAFOOD.png"
-                    alt=""
-                  />
-                  <h1 className="text-xs font-semibold text-gray-600 sm:text-sm md:text-base lg:text-lg">
-                    SEAFOOD
-                  </h1>
-                </div>
+        <NavLink to="/seafood">
+          {({ isActive }) => (
+            <div className="h-46 border-1 w-40 border-gray-200 bg-white  text-center shadow-2xl hover:border-red-200 hover:bg-red-200 md:rounded-sm md:border-[3px]">
+              <div className={isActive ? activeStyle : undefined}>
+                <img className="p-3" src="/assets/images/SEAFOOD.png" alt="" />
+                <h1 className="text-lg font-semibold text-gray-600">SEAFOOD</h1>
               </div>
-            )}
-          </NavLink>
-        </div>
+            </div>
+          )}
+        </NavLink>
         <div>
           <NavLink to="/soup">
             {({ isActive }) => (
-              <div className="border-1 border-gray-200 bg-white  text-center shadow-2xl hover:border-red-200 hover:bg-red-200 md:rounded-sm md:border-[3px]">
+              <div className="h-46 border-1 w-40 border-gray-200 bg-white  text-center shadow-2xl hover:border-red-200 hover:bg-red-200 md:rounded-sm md:border-[3px]">
                 <div className={isActive ? activeStyle : undefined}>
                   <img className="p-3" src="/assets/images/SUP.png" alt="" />
-                  <h1 className="text-xs font-semibold text-gray-600 sm:text-sm md:text-base lg:text-lg">
-                    SOUP
-                  </h1>
+                  <h1 className="text-lg font-semibold text-gray-600">SOUP</h1>
                 </div>
               </div>
             )}
@@ -88,16 +78,14 @@ export function HeaderNavigation() {
         <div>
           <NavLink to="/drink">
             {({ isActive }) => (
-              <div className="border-1 border-gray-200 bg-white  text-center shadow-2xl hover:border-red-200 hover:bg-red-200 md:rounded-sm md:border-[3px]">
+              <div className="h-46 border-1 w-40 border-gray-200 bg-white  text-center shadow-2xl hover:border-red-200 hover:bg-red-200 md:rounded-sm md:border-[3px]">
                 <div className={isActive ? activeStyle : undefined}>
                   <img
                     className="p-3"
                     src="/assets/images/MINUMAN.png"
                     alt=""
                   />
-                  <h1 className="text-xs font-semibold text-gray-600 sm:text-sm md:text-base lg:text-lg">
-                    DRINK
-                  </h1>
+                  <h1 className="text-lg font-semibold text-gray-600">DRINK</h1>
                 </div>
               </div>
             )}
