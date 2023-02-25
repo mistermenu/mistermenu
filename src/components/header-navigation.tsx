538f4c448148c8b29f5f0c35f9cb3c9952a29808
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-25 18:47:48 +0000

    feat: add <Link> to cart icon

--- src/components/header-navigation.tsx
+++ src/components/header-navigation.tsx
@@ -13,12 +13,14 @@ export function HeaderNavigation() {
           <Link to="/">
             <h1 className="font-rocksalt text-red-mistermenu">MisterMenu</h1>
           </Link>
-          <button className="relative flex h-12 w-12 items-center justify-center rounded-lg bg-red-mistermenu transition duration-300 ease-in-out hover:bg-orange-400">
-            <FaShoppingCart className="text-2xl text-white " />
-            <div className="outline-red absolute top-0 right-0 flex h-6 w-6 translate-x-1/4 -translate-y-1/4 items-center justify-center rounded-full bg-gray-400 font-sans text-white outline outline-2">
-              {cartQuantity}
-            </div>
-          </button>
+          <Link to="/cart">
+            <button className="relative flex h-12 w-12 items-center justify-center rounded-lg bg-red-mistermenu transition duration-300 ease-in-out hover:bg-orange-400">
+              <FaShoppingCart className="text-2xl text-white " />
+              <div className="outline-red absolute top-0 right-0 flex h-6 w-6 translate-x-1/4 -translate-y-1/4 items-center justify-center rounded-full bg-gray-400 font-sans text-white outline outline-2">
+                {cartQuantity}
+              </div>
+            </button>
+          </Link>
         </div>
       </div>
 
