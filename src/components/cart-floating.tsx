8ff330f7ff46192d5f47966051f78cb114377b5c
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-26 09:44:38 +0800

    floating cart to show only after 90 pixels Y-scroll

--- src/components/cart-floating.tsx
+++ src/components/cart-floating.tsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 export function FloatingCart() {
+  const { cartQuantity } = useShoppingCart();
   return (
     <Link to="/cart">
       <div className="fixed bottom-0 left-0 w-full p-3">
         <div className="static rounded-md bg-red-mistermenu hover:bg-red-900">
           <h1 className="p-3 text-center text-xl text-white">
-            Products in Cart
+            Products in Cart ({cartQuantity})
           </h1>
         </div>
       </div>
